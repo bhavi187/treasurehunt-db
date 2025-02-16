5b925591eea19b32e2bdf84095ce75faabe4cbb9
bhavi187 <vbhavishya05@gmail.com>
2025-02-16 08:28:05 -0500

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,7 +3,12 @@ def run_quiz():
         "Python": "What language is widely used for AI and web development?",
         "JavaScript": "What language is essential for front-end web development?",
         "Docker": "What tool is commonly used for containerization?",
-        "AI": "What field involves creating machines with human-like intelligence?"
+        "AI": "What field involves creating machines with human-like intelligence?",
+        "HTTPS": "Which protocol is used for secure data transfer over the web?",
+        "Binary" : "Which search algorithm is used in balanced binary trees with time complexity O(log n)?",
+        "Bash" : "What is the default shell for most Linux distributions?",
+        "Hashmap" :"What is the data structure where elements are stored in key-value pairs?",
+
     }
     
     correct_count = 0
